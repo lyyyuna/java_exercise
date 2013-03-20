7be233cee8bc3c5f3aac35112c97925cffc95877
lyyyuna <lyyyuna@gmail.com>
2013-03-20 18:53:55 +0800

    holding

---
+++ holding/IterableClass.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class IterableClass implements Iterable<String> {
+    protected String[] words = (
+        "and that is how " + "we knwo the earthto.").split(" ");
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            private int index = 0;
+            public boolean hasNext() {
+                return index < words.length;
+            }
+            public String next() { return words[index++];}
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+    public static void main(String[] args) {
+        for (String s : new IterableClass())
+            System.out.println(s + " ");
+    }
+}
